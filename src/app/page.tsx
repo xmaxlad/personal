b19cd97cc6a32b3ca981acaab671434b0d39b385
Kamal <kamalproacc@gmail.com>
2024-11-22 15:41:45 +0530

    basic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,77 @@
+import Link from "next/link" 
 import Image from "next/image";
+import {clsx} from 'clsx'  
+
+const Interests = [
+  {
+    heading:"interests : ",
+    content:"startups, edtech, gig economy, qc"
+  },
+  {
+    heading:"dev stack i know : ",
+    content:"nextjs(along with prisma, tailwind, postgresql), solidity"
+  },
+  {
+    heading:"deeptech that excites me : ",
+    content:"applied crypto, zk, privacy, matching algorithms"
+  },
+  {
+    heading:"been at : ",
+    content:"undergrad @iitr, tcs summer school @iitd" 
+  },
+]
+
+const pages = ['twocents','music','random'] 
+
+const contacts = [
+  {
+    where:'linkedin',
+    link:'https://www.linkedin.com/in/kamalsharma01/' 
+  },
+  {
+    where:'twt',
+    link:'https://x.com/0xmaxlad'
+  }
+]
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="m-6 font-mono"> 
+    <div className="text-2xl py-1">kamal sharma here</div> 
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-row">
+      {pages.map((page,idx) => (    
+        <div className='px-2 hover:underline w-fit' key={idx}><Link href={`/${page}`}>{page}</Link></div> 
+      ))}
+      </div>
+
+      <Image src='/self.jpeg' alt="Kamal" width={220} height={180} className="py-1"></Image> 
+      <div className="flex flex-col"> 
+        <div className="text-base">currently : building <Link href="https://www.sumlearn.com/" className="hover:underline">sumlearn.com</Link>  </div>     
+      </div>
+      {Interests.map(interest => (
+        <div className="flex flex-col py-2">
+          <div className="text-base">{interest.heading}</div>
+          <div className="text-sm">{interest.content}</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      ))}  
+
+      <div className="flex flex-row py-2">
+        {
+          contacts.map((contact,idx) => (
+            <div className="px-2 hover:underline w-fit" key={idx}> 
+              <Link href={contact.link} passHref legacyBehavior>
+              <a target="_blank">{contact.where}</a> 
+              </Link>
+            </div>
+          ))
+        }
+      </div> 
+
+      <div>
+        mail kamal@sumlearn.com kamalonlywork@gmail.com
+      </div>
+
+    </div> 
   );
 }
---
+++ src/app/random/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <div className='font-mono text-xl m-6'>
+            <h2>WIP</h2> 
+        </div>
+    )
+}
